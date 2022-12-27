6617a8fcb5c918dc154c6f95b7dd80ed76fc9538
Abu bakar <mdabubakarsiddik594@gmail.com>
2022-12-27 11:53:48 +0600

    add Delete

--- lib/pages/darft.dart
+++ lib/pages/darft.dart
@@ -15,6 +15,7 @@ class _DarftPageState extends State<DarftPage> {
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
+      
       myDarftList = Hive.box<Model>('DarftData').values.toList().cast<Model>();
       setState(() {});
     });
@@ -30,6 +31,70 @@ class _DarftPageState extends State<DarftPage> {
       appBar: AppBar(
         title: const Text('DarftData'),
       ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: ListView.builder(
+                  itemCount: myDarftList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ExpansionTile(
+                        title: Text(myDarftList[index].title),
+                        leading: SizedBox(
+                            height: 71,
+                            width: 71,
+                            child: Card(
+                                child: Image.network(
+                              myDarftList[index].image,
+                              fit: BoxFit.cover,
+                            ))),
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  IconButton(onPressed: (){
+                               
+                           
+                                      setState(() {
+                                      deletRxMedicinItem(myDarftList[index].id);
+                                        
+                                      });
+                                    
+                             
+
+
+                                  }, icon: Icon(Icons.delete)),
+                                  IconButton(onPressed: (){
+                                    Navigator.pop(context);
+                                  }, icon: Icon(Icons.details)),
+                                ],
+                              )
+                            ],
+                      ),
+                    );
+                  }))
+        ],
+      ),
     );
+  }
+   deletRxMedicinItem(int id) {
+    final box =Hive.box<Model>('DarftData');
+
+    final Map<dynamic, Model> deliveriesMap = box.toMap();
+    dynamic desiredKey;
+    deliveriesMap.forEach((key, value) {
+      if (value.id == id){
+          
+         desiredKey = key;
+           
+        
+         }
+    });
+    
+    box.delete(desiredKey);
+
+    print(desiredKey);
+      
+    
   }
 }
